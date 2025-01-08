162fd709fe51ed93c73cfc4f5253464a6d3bf3de
jean de dieu <jeandedieusessou@gmail.com>
2025-01-08 14:23:12 +0000

    fix profil problem

--- src/app/_components/Profile.tsx
+++ src/app/_components/Profile.tsx
@@ -8,47 +8,45 @@ import { Section } from "./Section";
 
 export const Profile = () => {
   return (
-    <Section>
-      {/* about */}
-      <div className="space-y-2">
-        <div className="mb-4 flex items-center justify-center gap-6">
-          <div>
-            <img
-              src="https://avatars.githubusercontent.com/u/141136058?v=4"
-              alt="profile"
-              className="w-24 h-24 rounded-full"
-            />
-          </div>
-          <div>
-            <h1 className="text-md md:text-2xl font-bold">
-              Hi, I'm Jean de Dieu
-            </h1>
-            <p className="text-xs md:text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              quos.
-            </p>
-          </div>
+    <Section className="py-8">
+      <div className="flex items-start gap-6 ">
+        <div className="flex items-center justify-center">
+          <img
+            src="https://avatars.githubusercontent.com/u/141136058?v=4"
+            alt=""
+            className="w-24 h-24 rounded-full"
+          />
         </div>
-        <div className="flex gap-4 mt-6 mb-8">
-          <Link
-            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
-            href="https://github.com/johnOfGod33"
-          >
-            <Github size={12} />
-          </Link>
-          <Link
-            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
-            href="https://linkedin.com/in/jean-de-dieu-sessou-260897292"
-          >
-            <Linkedin size={12} />
-          </Link>
-          <Link
-            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
-            href="https://twitter.com/JohnKamiCode"
-          >
-            <Twitter size={12} />
-          </Link>
+        {/* about */}
+        <div className="space-y-2">
+          <h1 className="text-md md:text-2xl font-bold">
+            Hi, I'm Jean de Dieu
+          </h1>
+          <p className="text-gray-500 max-w-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quos.
+          </p>
         </div>
+      </div>
+      <div className="flex gap-4 mt-6">
+        <Link
+          className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          href="https://github.com/johnOfGod33"
+        >
+          <Github size={12} />
+        </Link>
+        <Link
+          className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          href="https://linkedin.com/in/jean-de-dieu-sessou-260897292"
+        >
+          <Linkedin size={12} />
+        </Link>
+        <Link
+          className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          href="https://twitter.com/JohnKamiCode"
+        >
+          <Twitter size={12} />
+        </Link>
       </div>
     </Section>
   );
